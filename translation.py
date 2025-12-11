4120aa40bf270fc600e4c2d5fc35700005718f9f
Van Manh Le <manhpruong@gmail.com>
2025-12-10 17:15:05 -0800

    Resolved merge conflicts and updated all files

--- translation.py
+++ translation.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from googletrans import Translator
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
@@ -9,25 +7,3 @@ def translate_Vi_En(viet_text):
     translator = Translator()
     translation = translator.translate(viet_text, src='vi', dest='en')
     return translation.text
-=======
-from googletrans import Translator
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
-
-
-def translate_Vi_En(viet_text):
-    translator = Translator()
-    translation = translator.translate(viet_text, src='vi', dest='en')
-    return translation.text
->>>>>>> 57c02483a769b2b1c793f10614ac540529af5839
-=======
-from googletrans import Translator
-import sys
-sys.stdout.reconfigure(encoding='utf-8')
-
-
-def translate_Vi_En(viet_text):
-    translator = Translator()
-    translation = translator.translate(viet_text, src='vi', dest='en')
-    return translation.text
->>>>>>> 57c02483a769b2b1c793f10614ac540529af5839
